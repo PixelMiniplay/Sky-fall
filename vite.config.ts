517d6beabc0ac8ed3bd36dbbe02a6af33777b311
PixelMini play <itzdhirajchoudhary07@gmail.com>
2026-05-28 23:59:11 +0530

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Sky-fall/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
